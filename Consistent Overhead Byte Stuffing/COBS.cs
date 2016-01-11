a458ee77c477dda61c1e135a5c6f72124f1a457b
rockthethird <rockthethird@gmail.com>
2016-01-11 14:11:22 -0500

    Initial Code Commit

---
+++ Consistent Overhead Byte Stuffing/COBS.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consistent_Overhead_Byte_Stuffing {
+	/// <summary>
+	/// Consistent Overhead Byte Stuffing is an encoding that removes all 0 
+	/// bytes from arbitrary binary data. The encoded data consists only of 
+	/// bytes with values from 0x01 to 0xFF. This is useful for preparing data 
+	/// for transmission over a serial link (RS-232 or RS-485 for example), as 
+	/// the 0 byte can be used to unambiguously indicate packet boundaries. 
+	/// 
+	/// COBS also has the advantage of adding very little overhead (at least 1 
+	/// byte, plus up to an additional byte per 254 bytes of data). For 
+	/// messages smaller than 254 bytes, the overhead is constant.
+	/// </summary>
+	public static class COBS {
+		/// <summary>
+		/// The encoded data will contain only values from 0x01 to 0xFF.
+		/// </summary>
+		/// <param name="Input">An array up to 254 bytes in length</param>
+		/// <returns>Returns the encoded input</returns>
+		public static IEnumerable<byte> Encode(IEnumerable<byte> Input) {
+			if (Input == null)
+				return null;
+
+			if (Input.Count() > 254)
+				throw new ArgumentOutOfRangeException("Input length must not exceed 254 bytes");
+
+			var result = new List<byte>();
+			int distanceIndex = 0;
+			byte distance = 1;  // Distance to next zero
+
+			foreach (var i in Input) {
+				// If we encounter a zero (the frame delimiter)
+				if (i == 0) {
+					// Write the value of the distance to the next zero back in output where we last saw a zero
+					result.Insert(distanceIndex, distance);
+
+					// Set the distance index to the latest index plus one
+					distanceIndex = (byte)result.Count;
+
+					// Reset the value which indicates the distance to the next zero (the frame delimiter)
+					distance = 1;
+				}
+				else {
+					// Otherwise simply add the next value to the result
+					result.Add(i);
+
+					// Increment the distance to the next zero
+					distance++;
+
+					// Check for maximum distance value
+					if (distance == 0xFF) {
+						// Set the distance variable to its maximum value
+						result.Insert(distanceIndex, distance);
+
+						// Set the distance index to the latest index plus one
+						distanceIndex = (byte)result.Count;
+
+						// Reset the value which indicates the distance to the next zero (the frame delimiter)
+						distance = 1;
+					}
+				}
+			}
+
+			// If the packet hasn't reached the maximum size
+			if(result.Count != 255 && result.Count > 0)
+				// Add the last distance variable
+				result.Insert(distanceIndex, distance);
+
+			// Return with the result
+			return result;
+		}
+
+		/// <summary>
+		/// The decoded data will be restored with all zeros which were removed
+		/// during the decoding process.
+		/// </summary>
+		/// <param name="Input">A COBS encoded array</param>
+		/// <returns>Returns the decoded input</returns>
+		public static IEnumerable<byte> Decode(IEnumerable<byte> Input) {
+			if (Input == null)
+				return null;
+
+			if (Input.Count() > 254)
+				throw new ArgumentOutOfRangeException("Input length must not exceed 254 bytes");
+
+			var input = Input.ToArray();
+			var result = new List<byte>();
+			int distanceIndex = 0;
+			byte distance = 1;  // Distance to next zero
+
+			// Continue decoding which the next index is valid
+			while (distanceIndex < input.Length) {
+				// Get the next distance value
+				distance = input[distanceIndex];
+
+				// Ensure the input is formatted correctly (distanceIndex + distance)
+				if (input.Length < distanceIndex + distance) {
+					Trace.WriteLine("Consistent Overhead Byte Stuffing failed to parse an input.");
+					return new List<byte>();
+				}
+
+				// Add the range of byte up to the next zero
+				if (distance > 1) {
+					for (byte i = 1; i < distance; i++)
+						result.Add(input[distanceIndex + i]);
+				}
+
+				// Determine the next distance index (doing this here assists the below if)
+				distanceIndex += distance;
+
+				// Add the original zero back
+				if(distance < 0xFF && distanceIndex < input.Length)
+					result.Add(0);
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Stuffs "length" bytes of data at the location pointed to by
+		/// "input", writing the output to the location pointed to by
+		/// "output". Returns the number of bytes written to "output".
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="length"></param>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public static int cobs_encode(ref byte[] input, int length, ref byte[] output) {
+			int read_index = 0;
+			int write_index = 1;
+
+			int code_index = 0;
+			byte distance = 1;
+
+			while (read_index < length) {
+				// If we encounter a zero for the current value of the input
+				if (input[read_index] == 0) {
+					// Write the value of the distance to the next zero back in output where we last saw a zero
+					output[code_index] = distance;
+
+					// Set the distance index to the latest index plus one
+					code_index = write_index++;
+
+					// Reset the distance
+					distance = 1;
+
+					// Keep the read and write indexes the same
+					read_index++;
+				}
+				else {
+					// Simply copy the value over from the input (increment the indexes up one value)
+					output[write_index++] = input[read_index++];
+
+					// Increment the distance
+					distance++;
+
+					// If the distance reaches maximum valve
+					if (distance == 0xFF) {
+						// Set the distance variable to its maximum value
+						output[code_index] = distance;
+
+						// Set the distance index to the latest index plus one
+						code_index = write_index++;
+
+						// Reset the distance
+						distance = 1;
+					}
+				}
+			}
+
+			if(code_index != 255 && output.Count() > 0)
+				output[code_index] = distance;
+
+			return write_index;
+		}
+
+		/// <summary>
+		/// Unstuffs "length" bytes of data at the location pointed to by
+		/// "input", writing the output * to the location pointed to by
+		/// "output". Returns the number of bytes written to "output" if
+		/// "input" was successfully unstuffed, and 0 if there was an
+		/// error unstuffing "input".
+		/// </summary>
+		/// <param name="input"></param>
+		/// <param name="length"></param>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public static int cobs_decode(ref byte[] input, int length, ref byte[] output) {
+			int read_index = 0;
+			int write_index = 0;
+			byte distance;
+			byte i;
+
+			while (read_index < length) {
+				// Copy the current input value to the distance value
+				distance = input[read_index];
+
+				// If the index of the next distance value is greater than the length of the input
+				// AND the distance is not equal to one
+				if (read_index + distance > length && distance != 1) {
+					return 0;
+				}
+
+				// Increment to the next not zero value
+				read_index++;
+
+				// Copy the input to the output for the distance
+				for (i = 1; i < distance; i++) {
+					output[write_index++] = input[read_index++];
+				}
+
+				// Determine if the 
+				if (distance != 0xFF && read_index != length) {
+					output[write_index++] = Convert.ToByte('\0');
+				}
+			}
+
+			return write_index;
+		}
+	}
+}
---
+++ Consistent Overhead Byte Stuffing/Test.cs
@@ -0,0 +1,278 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consistent_Overhead_Byte_Stuffing {
+	[TestClass]
+	public class Test {
+		static byte[] decoded1 = new byte[] { 0 };
+		static byte[] decoded2 = new byte[] { 0, 0 };
+		static byte[] decoded3 = new byte[] { 11, 22, 0, 33 };
+		static byte[] decoded4 = new byte[] { 11, 22, 33, 44 };
+		static byte[] decoded5 = new byte[] { 11, 0, 0, 0 };
+		static byte[] decoded6 = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254 };
+
+		static byte[] encoded1 = new byte[] { 1, 1 };
+		static byte[] encoded2 = new byte[] { 1, 1, 1 };
+		static byte[] encoded3 = new byte[] { 3, 11, 22, 2, 33 };
+		static byte[] encoded4 = new byte[] { 5, 11, 22, 33, 44 };
+		static byte[] encoded5 = new byte[] { 2, 11, 1, 1, 1 };
+		static byte[] encoded6 = new byte[] { 255, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254 };
+
+		[TestMethod]
+		public void TestEncoding() {
+			var encodeTest = new byte[1000];
+
+			// Jacques Fortier implementation (slight modifications for C#)
+			COBS.cobs_encode(ref decoded1, decoded1.Count(), ref encodeTest);
+			Assert.IsTrue(encoded1.SequenceEqual(encodeTest.Take(encoded1.Count())));
+
+			COBS.cobs_encode(ref decoded2, decoded2.Count(), ref encodeTest);
+			Assert.IsTrue(encoded2.SequenceEqual(encodeTest.Take(encoded2.Count())));
+
+			COBS.cobs_encode(ref decoded3, decoded3.Count(), ref encodeTest);
+			Assert.IsTrue(encoded3.SequenceEqual(encodeTest.Take(encoded3.Count())));
+
+			COBS.cobs_encode(ref decoded4, decoded4.Count(), ref encodeTest);
+			Assert.IsTrue(encoded4.SequenceEqual(encodeTest.Take(encoded4.Count())));
+
+			COBS.cobs_encode(ref decoded5, decoded5.Count(), ref encodeTest);
+			Assert.IsTrue(encoded5.SequenceEqual(encodeTest.Take(encoded5.Count())));
+
+			COBS.cobs_encode(ref decoded6, decoded6.Count(), ref encodeTest);
+			Assert.IsTrue(encoded6.SequenceEqual(encodeTest.Take(encoded6.Count())));
+
+			// IEnumerable method
+			Assert.IsTrue(encoded1.SequenceEqual(COBS.Encode(decoded1)));
+			Assert.IsTrue(encoded2.SequenceEqual(COBS.Encode(decoded2)));
+			Assert.IsTrue(encoded3.SequenceEqual(COBS.Encode(decoded3)));
+			Assert.IsTrue(encoded4.SequenceEqual(COBS.Encode(decoded4)));
+			Assert.IsTrue(encoded5.SequenceEqual(COBS.Encode(decoded5)));
+			Assert.IsTrue(encoded6.SequenceEqual(COBS.Encode(decoded6)));
+		}
+
+		[TestMethod]
+		public void TestEncodingNull() {
+			Assert.AreEqual(null, COBS.Encode(null));
+		}
+
+		[TestMethod]
+		public void TestEncodingEmpty() {
+			Assert.IsTrue(new List<byte>().SequenceEqual(COBS.Encode(new List<byte>())));
+		}
+
+		[TestMethod]
+		public void TestEncodingTooLarge() {
+			try {
+				// Create an array one byte too large
+				var outOfRangeArray = new byte[255];
+
+				// Attempt to encode the array
+				COBS.Encode(outOfRangeArray);
+
+				// If the code does NOT throw the exception the method failed
+				Assert.Fail();
+			}
+			catch (ArgumentOutOfRangeException) {
+				// If the exception was caught then the test passed
+			}
+		}
+
+		[TestMethod]
+		public void TestDecoding() {
+			var decodeTest = new byte[1000];
+
+			// Jacques Fortier implementation (slight modifications for C#)
+			COBS.cobs_decode(ref encoded1, encoded1.Count(), ref decodeTest);
+			Assert.IsTrue(decoded1.SequenceEqual(decodeTest.Take(decoded1.Count())));
+
+			COBS.cobs_decode(ref encoded2, encoded2.Count(), ref decodeTest);
+			Assert.IsTrue(decoded2.SequenceEqual(decodeTest.Take(decoded2.Count())));
+
+			COBS.cobs_decode(ref encoded3, encoded3.Count(), ref decodeTest);
+			Assert.IsTrue(decoded3.SequenceEqual(decodeTest.Take(decoded3.Count())));
+
+			COBS.cobs_decode(ref encoded4, encoded4.Count(), ref decodeTest);
+			Assert.IsTrue(decoded4.SequenceEqual(decodeTest.Take(decoded4.Count())));
+
+			COBS.cobs_decode(ref encoded5, encoded5.Count(), ref decodeTest);
+			Assert.IsTrue(decoded5.SequenceEqual(decodeTest.Take(decoded5.Count())));
+
+			COBS.cobs_decode(ref encoded6, encoded6.Count(), ref decodeTest);
+			Assert.IsTrue(decoded6.SequenceEqual(decodeTest.Take(decoded6.Count())));
+
+			Assert.IsTrue(decoded1.SequenceEqual(COBS.Decode(encoded1)));
+			Assert.IsTrue(decoded2.SequenceEqual(COBS.Decode(encoded2)));
+			Assert.IsTrue(decoded3.SequenceEqual(COBS.Decode(encoded3)));
+			Assert.IsTrue(decoded4.SequenceEqual(COBS.Decode(encoded4)));
+			Assert.IsTrue(decoded5.SequenceEqual(COBS.Decode(encoded5)));
+			Assert.IsTrue(decoded6.SequenceEqual(COBS.Decode(encoded6)));
+		}
+
+		[TestMethod]
+		public void TestDecodingNull() {
+			Assert.AreEqual(null, COBS.Decode(null));
+		}
+
+		[TestMethod]
+		public void TestDecodingEmpty() {
+			Assert.IsTrue(new List<byte>().SequenceEqual(COBS.Decode(new List<byte>())));
+		}
+
+		[TestMethod]
+		public void TestDecodingTooLarge() {
+			try {
+				// Create an array one byte too large
+				var outOfRangeArray = new byte[255];
+
+				// Attempt to encode the array
+				COBS.Decode(outOfRangeArray);
+
+				// If the code does NOT throw the exception the method failed
+				Assert.Fail();
+			}
+			catch (ArgumentOutOfRangeException) {
+				// If the exception was caught then the test passed
+			}
+		}
+
+		[TestMethod]
+		public void TestEncodingThenDecoding() {
+			var encodeTest = new byte[1000];
+			var decodeTest = new byte[1000];
+
+			// Jacques Fortier implementation (slight modifications for C#)
+			COBS.cobs_encode(ref decoded1, decoded1.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded1.Count(), ref decodeTest);
+			Assert.IsTrue(decoded1.SequenceEqual(decodeTest.Take(decoded1.Count())));
+
+			COBS.cobs_encode(ref decoded2, decoded2.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded2.Count(), ref decodeTest);
+			Assert.IsTrue(decoded2.SequenceEqual(decodeTest.Take(decoded2.Count())));
+
+			COBS.cobs_encode(ref decoded3, decoded3.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded3.Count(), ref decodeTest);
+			Assert.IsTrue(decoded3.SequenceEqual(decodeTest.Take(decoded3.Count())));
+
+			COBS.cobs_encode(ref decoded4, decoded4.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded4.Count(), ref decodeTest);
+			Assert.IsTrue(decoded4.SequenceEqual(decodeTest.Take(decoded4.Count())));
+
+			COBS.cobs_encode(ref decoded5, decoded5.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded5.Count(), ref decodeTest);
+			Assert.IsTrue(decoded5.SequenceEqual(decodeTest.Take(decoded5.Count())));
+
+			COBS.cobs_encode(ref decoded6, decoded6.Count(), ref encodeTest);
+			COBS.cobs_decode(ref encodeTest, encoded6.Count(), ref decodeTest);
+			Assert.IsTrue(decoded6.SequenceEqual(decodeTest.Take(decoded6.Count())));
+
+			Assert.IsTrue(decoded1.SequenceEqual(COBS.Decode(COBS.Encode(decoded1))));
+			Assert.IsTrue(decoded2.SequenceEqual(COBS.Decode(COBS.Encode(decoded2))));
+			Assert.IsTrue(decoded3.SequenceEqual(COBS.Decode(COBS.Encode(decoded3))));
+			Assert.IsTrue(decoded4.SequenceEqual(COBS.Decode(COBS.Encode(decoded4))));
+			Assert.IsTrue(decoded5.SequenceEqual(COBS.Decode(COBS.Encode(decoded5))));
+			Assert.IsTrue(decoded6.SequenceEqual(COBS.Decode(COBS.Encode(decoded6))));
+		}
+
+		/// <summary>
+		/// THESE TESTS ARE INVALID AND ABANDONED AND REMAIN FOR REFERENCE ONLY
+		/// 
+		/// An unidentified anomaly exists in which the order of the tests or
+		/// the surround code (i.e. other test changes) affects the timing.  
+		/// For example, if you move the block of code for the second test 
+		/// above the first, the times will move dramatically outside expected 
+		/// variances.  GC.Collect() was added as an attempt to remedy this 
+		/// anomaly with little to no effect.
+		/// 
+		/// The order of the tests were chosen so that the time for the Jacques
+		/// Fortier implementation is at its fastest.
+		/// 
+		/// Any insight for why this happens would be appreciated.
+		/// </summary>
+		//[TestMethod]
+		public void TestEncodeSpeed() {
+			var duration1 = new Stopwatch();
+			var duration2 = new Stopwatch();
+
+			// Prepare for performance testing
+			GC.Collect();
+
+			// Time the IEnumerable version
+			IEnumerable<byte> result1;
+			duration1.Start();
+			result1 = COBS.Encode(decoded6);
+			duration1.Stop();
+
+			// Time the Jacques Fortier implementation (slight modifications for C#)
+			var encodeTest = new byte[255];
+			duration2.Start();
+			COBS.cobs_encode(ref decoded6, decoded6.Count(), ref encodeTest);
+			duration2.Stop();
+
+			// Display the results
+			Console.WriteLine(String.Format("IEnumerable: {0} ticks ({1}) | Jacques Fortier: {2} ticks ({3})", duration1.ElapsedTicks, duration1.Elapsed, duration2.ElapsedTicks, duration2.Elapsed));
+
+			// Ensure IEnumerable encode/decode succeeded
+			Assert.IsTrue(encoded6.SequenceEqual(result1));
+
+			// Ensure Jacques Fortier encode/decode succeeded
+			Assert.IsTrue(encoded6.SequenceEqual(encodeTest));
+
+			// Test if the IEnumerable friendly implementation is faster
+			Assert.IsTrue(duration1.ElapsedTicks < duration2.ElapsedTicks);
+		}
+
+		/// <summary>
+		/// THESE TESTS ARE INVALID AND ABANDONED AND REMAIN FOR REFERENCE ONLY
+		/// 
+		/// An unidentified anomaly exists in which the order of the tests or
+		/// the surround code (i.e. other test changes) affects the timing.  
+		/// For example, if you move the block of code for the second test 
+		/// above the first, the times will move dramatically outside expected 
+		/// variances.  GC.Collect() was added as an attempt to remedy this 
+		/// anomaly with little to no effect.
+		/// 
+		/// The order of the tests were chosen so that the time for the Jacques
+		/// Fortier implementation is at its fastest.
+		/// 
+		/// Any insight for why this happens would be appreciated.
+		/// </summary>
+		//[TestMethod]
+		public void TestDecodeSpeed() {
+			var duration1 = new Stopwatch();
+			var duration2 = new Stopwatch();
+
+			// Prepare for performance testing
+			GC.Collect();
+
+			// Time the IEnumerable version
+			IEnumerable<byte> result1;
+			duration1.Restart();
+			result1 = COBS.Decode(encoded6);
+			duration1.Stop();
+
+			// Time the Jacques Fortier implementation (slight modifications for C#)
+			var decodeTest = new byte[254];
+			duration2.Restart();
+			COBS.cobs_decode(ref encoded6, encoded6.Count(), ref decodeTest);
+			duration2.Stop();
+
+			// Display the results
+			Console.WriteLine(String.Format("IEnumerable: {0} ticks ({1}) | Jacques Fortier: {2} ticks ({3})", duration1.ElapsedTicks, duration1.Elapsed, duration2.ElapsedTicks, duration2.Elapsed));
+
+			// Ensure IEnumerable encode/decode succeeded
+			Assert.IsTrue(decoded6.SequenceEqual(result1));
+
+			// Ensure Jacques Fortier encode/decode succeeded
+			Assert.IsTrue(decoded6.SequenceEqual(decodeTest.Take(decoded6.Count())));
+
+			// Test if the IEnumerable friendly implementation is faster
+			Assert.IsTrue(duration1.ElapsedTicks < duration2.ElapsedTicks);
+		}
+	}
+}
